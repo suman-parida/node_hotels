d773729a879ae800a2bd1289466522636c2ca0cf
suman kumar parida <sumanparida42642@gmail.com>
2025-12-29 12:50:43 +0530

    connect to online server and localhost

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// MongoDB Atlas connection string from environment variables
-//const mongoURL = process.env.MONGODB_URI || 'mongodb+srv://helloworld:suman1234@cluster0.zjtvplw.mongodb.net/hotels?retryWrites=true&w=majority';
+// MongoDB connection - you can switch between local and Atlas
+const useLocal = true; // Set to false to use MongoDB Atlas
 
-// Connection options for Mongoose 9.x (removed deprecated options)
+let mongoURL;
+if (useLocal) {
+    // Local MongoDB connection
+    mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+} else {
+    // MongoDB Atlas connection
+    mongoURL = process.env.MONGODB_URI || 'mongodb+srv://helloworld:suman1234@cluster0.zjtvplw.mongodb.net/hotels?retryWrites=true&w=majority';
+}
+
+// Connection options for Mongoose 9.x
 const connectionOptions = {
     serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
@@ -14,17 +23,29 @@ const connectionOptions = {
 // Connect to MongoDB
 mongoose.connect(mongoURL, connectionOptions)
     .then(() => {
-        console.log('Connected to MongoDB Atlas successfully');
+        console.log(`Connected to MongoDB ${useLocal ? 'locally' : 'Atlas'} successfully`);
         console.log('Database:', mongoose.connection.name);
+        console.log('Host:', mongoose.connection.host);
+        console.log('Port:', mongoose.connection.port);
     })
     .catch((err) => {
         console.error('MongoDB connection error:', err.message);
-        console.error('Please check:');
-        console.error('1. Your internet connection');
-        console.error('2. MongoDB Atlas cluster is running');
-        console.error('3. IP address is whitelisted in MongoDB Atlas');
-        console.error('4. Username and password are correct');
-        console.error('5. Database name exists');
+        if (useLocal) {
+            console.error('\nLocal MongoDB connection failed. Please check:');
+            console.error('1. MongoDB is installed and running locally');
+            console.error('2. MongoDB service is started');
+            console.error('3. Default port 27017 is available');
+            console.error('\nTo start MongoDB locally:');
+            console.error('- Windows: net start MongoDB (as administrator)');
+            console.error('- macOS/Linux: sudo systemctl start mongod');
+            console.error('- Or try: mongod --dbpath /path/to/your/db');
+        } else {
+            console.error('\nMongoDB Atlas connection failed. Please check:');
+            console.error('1. Your internet connection');
+            console.error('2. MongoDB Atlas cluster is running');
+            console.error('3. IP address is whitelisted in MongoDB Atlas');
+            console.error('4. Username and password are correct');
+        }
         process.exit(1);
     });
 
@@ -33,7 +54,7 @@ const db = mongoose.connection;
 
 // Connection events
 db.on('connected', () => {
-    console.log('MongoDB connected');
+    console.log('MongoDB connected successfully');
 });
 
 db.on('error', (err) => {
@@ -45,35 +66,3 @@ db.on('disconnected', () => {
 });
 
 module.exports = db;
-
-
-// const mongoose = require('mongoose');
-
-// const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
-
-// // Connect to MongoDB
-// mongoose.connect(mongoURL)
-//     .then(() => {
-//         console.log('Connected to MongoDB server');
-//     })
-//     .catch((err) => {
-//         console.log('MongoDB connection error:', err);
-//     });
-
-// // Connection object
-// const db = mongoose.connection;
-
-// // Connection events
-// db.on('connected', () => {
-//     console.log('MongoDB connected');
-// });
-
-// db.on('error', (err) => {
-//     console.log('MongoDB connection error:', err);
-// });
-
-// db.on('disconnected', () => {
-//     console.log('MongoDB disconnected');
-// });
-
-// module.exports = db;
